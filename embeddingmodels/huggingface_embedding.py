bf426f656bea0d38c8e483df0742bb4fc3b37958
Ishan <chhaparwalishan@email.com>
2026-07-18 22:48:31 +0530

    HuggingFace Embeddings Implementaion

---
+++ embeddingmodels/huggingface_embedding.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+)
+
+texts = [
+    "Hello this is Ishan Chhaparwal",
+    "Hello Your name is YouTube",
+    "And you all are very beautiful"
+]
+
+vector = embedding.embed_documents(texts)
+print(vector)
